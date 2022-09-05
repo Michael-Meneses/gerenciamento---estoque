94f5ade6e4e8fd0da1cc8b6e24c5c0216b822a67
Michael Meneses <michaelfullstack@gmail.com>
2022-09-05 20:53:57 -0300

    Estrutura do projeto

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('[data-action="menu"]').forEach( (item) => {
+    item.addEventListener('click', () => {
+        content.innerHTML = window[item.dataset.content]();
+
+
+} );
+})
+
